64dba16445b2c2c4655b74b1e2bb5036fd259845
Fedor Sakhabutdinov <fedorsah@gmail.com>
2022-11-01 22:56:30 +0300

    Corrected unmerged changes

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -7,7 +7,6 @@ end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
 	local signs = {
@@ -88,7 +87,6 @@ M.on_attach = function(client, bufnr)
 	illuminate.on_attach(client)
 end
 
-<<<<<<< HEAD
 local function lsp_keymaps(bufnr)
 	local opts = { noremap = true, silent = true }
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
@@ -132,6 +130,4 @@ end
 
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
-=======
->>>>>>> master
 return M
--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,39 +1,6 @@
-<<<<<<< HEAD
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok then
-	return
-end
-
-local lspconfig = require("lspconfig")
-
 local servers = {
   "jsonls", "sumneko_lua", "solargraph", "pyright", "clangd", "cmake", "bashls",
   "eslint", "marksman"
-}
-
-lsp_installer.setup({
-	ensure_installed = servers,
-})
-
-for _, server in pairs(servers) do
-	local opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
-	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
-	if has_custom_opts then
-		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
-	end
-=======
-local servers = {
-	"sumneko_lua",
-	-- "cssls",
-	-- "html",
-	-- "tsserver",
-	"pyright",
-	-- "bashls",
-	"jsonls",
-	-- "yamlls",
 }
 
 local settings = {
@@ -75,6 +42,5 @@ for _, server in pairs(servers) do
 		opts = vim.tbl_deep_extend("force", conf_opts, opts)
 	end
 
->>>>>>> master
 	lspconfig[server].setup(opts)
 end
